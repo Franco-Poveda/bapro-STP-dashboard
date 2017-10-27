1876da9472e7f59bbe2f989b8e7af575616d133e
Franco-Poveda <franco.poveda@gmail.com>
2017-10-27 03:56:48 -0300

    scripts

---
+++ server/install-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash    
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt-get install -y docker-ce
